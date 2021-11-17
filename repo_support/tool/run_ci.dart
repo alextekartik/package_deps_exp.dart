1e38e2f389e952d0ef4ba981d741fd99258e0eae
alex <alex@tekartik.com>
2021-11-17 12:13:21 +0100

    ignore non dart2_3 project on windows

--- repo_support/tool/run_ci.dart
+++ repo_support/tool/run_ci.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
 import 'package:dev_test/package.dart';
 import 'package:path/path.dart';
 
 Future main() async {
   for (var dir in [
-    'all_projects_dart2',
-    'all_projects_null_safety',
+    if (!Platform.isWindows) 'all_projects_dart2',
+    if (!Platform.isWindows) 'all_projects_null_safety',
     'all_projects_dart2_3',
   ]) {
     await packageRunCi(join('..', 'packages', dir),
