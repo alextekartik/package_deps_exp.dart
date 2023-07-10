ee126a1647f5618617db88c402fc6338014ea4c9
Alexandre Roux <alex@tekartik.com>
2023-07-10 16:56:07 +0200

    dart3a support

--- repo_support/tool/run_ci.dart
+++ repo_support/tool/run_ci.dart
@@ -1,5 +1,7 @@
 import 'package:dev_test/package.dart';
 import 'package:path/path.dart';
+import 'package:process_run/shell.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 Future main() async {
   for (var dir in [
@@ -7,5 +9,13 @@ Future main() async {
   ]) {
     await packageRunCi(join('..', 'packages', dir),
         options: PackageRunCiOptions(pubGetOnly: true));
+  }
+  if (dartVersion >= Version(3, 0, 0)) {
+    for (var dir in [
+      'all_projects_dart3a',
+    ]) {
+      await packageRunCi(join('..', 'packages', dir),
+          options: PackageRunCiOptions(pubGetOnly: true));
+    }
   }
 }
--- repo_support/tool/run_ci_flutter.dart
+++ repo_support/tool/run_ci_flutter.dart
@@ -1,12 +1,22 @@
 import 'package:dev_test/package.dart';
 import 'package:path/path.dart';
+import 'package:process_run/shell.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 Future main() async {
   for (var dir in [
     'all_projects_flutter_dart2_3',
-    'all_projects_flutter_dart3a',
   ]) {
     await packageRunCi(join('..', 'packages', dir),
         options: PackageRunCiOptions(pubGetOnly: true));
+  }
+
+  if (dartVersion >= Version(3, 0, 0)) {
+    for (var dir in [
+      'all_projects_flutter_dart3a',
+    ]) {
+      await packageRunCi(join('..', 'packages', dir),
+          options: PackageRunCiOptions(pubGetOnly: true));
+    }
   }
 }
