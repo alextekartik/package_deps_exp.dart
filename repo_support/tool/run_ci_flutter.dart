61353aad1ac7c0180899ac2967df6a776bbbbcd0
Alexandre Roux <alex@tekartik.com>
2023-07-10 13:20:22 +0200

    dart3a support

--- repo_support/tool/run_ci_flutter.dart
+++ repo_support/tool/run_ci_flutter.dart
@@ -4,6 +4,7 @@ import 'package:path/path.dart';
 Future main() async {
   for (var dir in [
     'all_projects_flutter_dart2_3',
+    'all_projects_flutter_dart3a',
   ]) {
     await packageRunCi(join('..', 'packages', dir),
         options: PackageRunCiOptions(pubGetOnly: true));
