889b62bcff8efd4ffebe13994c268f42d151f83b
Alexandre Roux <alex@tekartik.com>
2024-10-21 16:38:07 +0200

    add vertex ai

---
+++ packages/all_projects_flutter_dart3a/tool/tkpub_setup_menu.dart
@@ -0,0 +1,30 @@
+import 'package:dev_build/menu/menu_io.dart';
+import 'package:tekartik_prj_tktools/tkpub.dart';
+
+Future<void> main(List<String> args) async {
+  mainMenuConsole(args, () {
+    menu('once', () {
+      item('setup vertex ai', () async {
+        await tkPubDbAction((configDb) async {
+          var gitUrl = 'https://github.com/tekartik/firebase_vertex_ai.dart';
+          var package = TkPubDbPackage()
+            ..gitUrl.v = gitUrl
+            ..gitPath.v = 'vertex_ai';
+
+          package =
+              await configDb.setPackage('tekartik_firebase_vertex_ai', package);
+          write(package);
+          gitUrl = 'https://github.com/tekartik/firebase_flutter';
+          package = TkPubDbPackage()
+            ..gitUrl.v = gitUrl
+            ..gitPath.v = 'vertex_ai_flutter';
+
+          package = await configDb.setPackage(
+              'tekartik_firebase_vertex_ai_flutter', package);
+
+          write(package);
+        }, write: true, verbose: true);
+      });
+    });
+  });
+}
